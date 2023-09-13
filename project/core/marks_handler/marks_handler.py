72d2caf42d4854e9451fdef1285cd720243cf294
o_VVBaranov <baranov.vladislav@phystech.edu>
2023-09-14 00:13:44 +0300

    cделал try except и ещё что-то

--- project/core/marks_handler/marks_handler.py
+++ project/core/marks_handler/marks_handler.py
@@ -28,22 +28,25 @@ class MarksHandler(QObject):
 
     @pyqtSlot(MarkData)
     def create_mark(self, mark_info: MarkData):
-        geo_data = [mark_info.longitude, mark_info.latitude, mark_info.altitude]
-        coordinates_id = CoordinatesDto.create_coordinates(*geo_data)
-        mark_id = MarkDto.create_mark(coordinates_id=coordinates_id)
-        relating_object_id = RelatingObjectDto. \
-            create_relating_object(type_relating=mark_info.relating_type,
-                                   name=mark_info.relating_name)
+        try:
+            geo_data = [mark_info.longitude, mark_info.latitude, mark_info.altitude]
+            coordinates_id = CoordinatesDto.create_coordinates(*geo_data)
+            mark_id = MarkDto.create_mark(coordinates_id=coordinates_id)
+            relating_object_id = RelatingObjectDto. \
+                create_relating_object(type_relating=mark_info.relating_type,
+                                       name=mark_info.relating_name)
 
-        object_id = ObjectDto.create_object(mark_id=mark_id, name=mark_info.name,
-                                            object_type=mark_info.object_type,
-                                            relating_object_id=relating_object_id,
-                                            meta=mark_info.comment)
+            object_id = ObjectDto.create_object(mark_id=mark_id, name=mark_info.name,
+                                                object_type=mark_info.object_type,
+                                                relating_object_id=relating_object_id,
+                                                meta=mark_info.comment)
 
-        new_map_mark = CanvasMark(object_id, mark_info.name, mark_info.latitude,
-                                  mark_info.longitude, self.painter)
+            new_map_mark = CanvasMark(object_id, mark_info.name, mark_info.latitude,
+                                      mark_info.longitude, self.painter)
 
-        object_ = self.session.query(ObjectDto).get(object_id)
+            object_ = self.session.query(ObjectDto).get(object_id)
+        except Exception:
+            raise Exception(f'Не удалось создать отметку')
 
         self.all_marks.append(object_)
         self.map_marks.append(new_map_mark)
@@ -53,24 +56,31 @@ class MarksHandler(QObject):
 
     @pyqtSlot(MarkData)
     def update_mark(self, mark_info: MarkData):
-        object_ = self.session.query(ObjectDto).get(mark_info.obj_id)
+        try:
+            object_ = self.session.query(ObjectDto).get(mark_info.obj_id)
+            old_mark_id = object_.mark.id
+            old_relating_object_id = object_.relating_object.id
+        except Exception:
+            raise Exception(f'При обновлении отметки, не удалось '
+                            f'получить старые данные об отметке с id={mark_info.obj_id}')
 
-        old_mark_id = object_.mark.id
-        old_relating_object_id = object_.relating_object.id
-        MarkDto.delete_mark(old_mark_id)
-        RelatingObjectDto.delete_relating_object(old_relating_object_id)
+        try:
+            MarkDto.delete_mark(old_mark_id)
+            RelatingObjectDto.delete_relating_object(old_relating_object_id)
 
-        new_geo_data = [mark_info.longitude, mark_info.latitude, mark_info.altitude]
-        new_coordinates_id = CoordinatesDto.create_coordinates(*new_geo_data)
-        new_mark_id = MarkDto.create_mark(coordinates_id=new_coordinates_id)
-        new_relating_object_id = RelatingObjectDto. \
-            create_relating_object(type_relating=mark_info.relating_type,
-                                   name=mark_info.relating_name)
+            new_geo_data = [mark_info.longitude, mark_info.latitude, mark_info.altitude]
+            new_coordinates_id = CoordinatesDto.create_coordinates(*new_geo_data)
+            new_mark_id = MarkDto.create_mark(coordinates_id=new_coordinates_id)
+            new_relating_object_id = RelatingObjectDto. \
+                create_relating_object(type_relating=mark_info.relating_type,
+                                       name=mark_info.relating_name)
 
-        ObjectDto.update_object(object_id=mark_info.obj_id, new_mark_id=new_mark_id,
-                                new_name=mark_info.name, new_object_type=mark_info.object_type,
-                                new_relating_object_id=new_relating_object_id,
-                                new_meta=mark_info.comment)
+            ObjectDto.update_object(object_id=mark_info.obj_id, new_mark_id=new_mark_id,
+                                    new_name=mark_info.name, new_object_type=mark_info.object_type,
+                                    new_relating_object_id=new_relating_object_id,
+                                    new_meta=mark_info.comment)
+        except Exception:
+            raise Exception(f'Не удалось обновить данные отметки с id={mark_info.obj_id}')
 
         for mark in self.map_marks:
             if mark.id == mark_info.obj_id:
@@ -86,12 +96,15 @@ class MarksHandler(QObject):
     def delete_mark(self, object_id):
         del self.dict_map_database_marks[object_id]
 
-        for mark in self.map_marks:
-            if mark.id == object_id:
-                self.map_marks.remove(mark)
-                mark.remove()
-                self.removeMark.emit(object_id)
-                break
+        try:
+            for mark in self.map_marks:
+                if mark.id == object_id:
+                    self.map_marks.remove(mark)
+                    mark.remove()
+                    self.removeMark.emit(object_id)
+                    break
+        except Exception:
+            raise Exception(f'Ошибка удаления отметки с id={object_id}')
 
     @pyqtSlot(int)
     def remove_mark_from_database(self, object_id):
@@ -103,7 +116,9 @@ class MarksHandler(QObject):
         self.dict_map_database_marks[object_id].set_visibility(visibility)
 
     def put_all_marks(self):
+
         self.all_marks = ObjectDto.get_all_objects()
+
         self.map_marks = [CanvasMark(mark.id, mark.name, mark.mark.coordinates.latitude,
                                      mark.mark.coordinates.longitude, self.painter)
                           for mark in self.all_marks]
